554806905b96bf56a089f2b0cda2a5a0de1be2aa
Tim Cort <timcort@gmail.com>
2013-07-08 17:22:25 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UnderdogUpstart::Application.config.secret_key_base = '5e230714e500da21b1db0327b11ac9ed3e3fb4f19435faf5750cde9c6813e75e972d8d47aca21a0d4885a56fc913dd22305117866680c174fb4f2314db605fe8'
