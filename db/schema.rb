4164001a59e4cf7cd0e757a1f48bc17e0c5cc414
Tim Cort <timcort@gmail.com>
2013-10-22 17:05:47 +1000

    add line item model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131022061757) do
+ActiveRecord::Schema.define(version: 20131022070200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20131022061757) do
   create_table "carts", force: true do |t|
     t.integer  "user_id"
     t.datetime "purchased_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", force: true do |t|
+    t.integer  "cart_id"
+    t.integer  "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
